9257b9d1c875216618babf8644968eecb4f50626
Brooke Kullberg and Todd Aden <brooke+todd@>
2019-07-17 12:00:00 -0700

    move to add mstests

---
+++ mathletics/Quizmaster/Quizmaster.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using mathletics.Context;
+using System;
+
+namespace mathletics
+{
+    public class Quizmaster
+    {
+        Random random = new System.Random();
+
+        public List<Question> CreateQuiz()
+        {
+            List<Question> questions = new List<Question>();                        
+            
+
+            return questions;
+        }
+
+
+
+        public Question QuizAddition()
+        {
+            Question question = new Question();
+            int min = 1, max = 100;            
+            int value1 = random.Next(min, max);
+            int value2 = random.Next(min, max);
+            
+            question.Prompt = $"{value1} + {value2} =";
+            int one, two, three;
+            one = random.Next(min, max) + random.Next(min, max);
+            two = random.Next(min, max) + random.Next(min, max);
+            three = random.Next(min, max) + random.Next(min, max);
+            if (one == value1 + value2) { one++; }
+            if (two == value1 + value2) { two++; }
+            if (three == value1 + value2) { three++; }
+
+            question.Option1 = one.ToString();
+            question.Option2 = two.ToString();
+            question.Option3 = three.ToString();
+            
+            switch (random.Next(1, 3))
+            {
+                case 1:
+                    question.CorrectAnswer = 1;
+                    question.Option1 = (value1 + value2).ToString();    
+                break;
+                case 2:
+                    question.CorrectAnswer = 2;
+                    question.Option2 = (value1 + value2).ToString();    
+                break;
+                case 3:
+                    question.CorrectAnswer = 3;
+                    question.Option3 = (value1 + value2).ToString();    
+                break;
+            }                                
+            return question;
+        }
+
+
+
+    }
+}
+
